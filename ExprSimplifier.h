2a9080a14b707e3842f2e363a359c59a7ff60f95
Manasij Mukherjee <manasij7479@gmail.com>
2016-11-08 12:45:17 +0530

    More simplification methods

--- ExprSimplifier.h
+++ ExprSimplifier.h
@@ -21,7 +21,7 @@ std::pair<CVC4::Expr, bool> ShortCircuitBinaryAnd(CVC4::SmtEngine &SMT, CVC4::Ex
 std::pair<CVC4::Expr, bool> ShortCircuitBinaryOr(CVC4::SmtEngine &SMT, CVC4::Expr E) {
   if (E.getNumChildren() != 2)
     return {E, false};
-  assert(E.getKind() == CVC4::Kind::AND);
+  assert(E.getKind() == CVC4::Kind::OR);
   auto L = E.getChildren()[0];
   auto R = E.getChildren()[1];
   auto Result = SMT.query(L);
@@ -32,16 +32,47 @@ std::pair<CVC4::Expr, bool> ShortCircuitBinaryOr(CVC4::SmtEngine &SMT, CVC4::Exp
     return {L, true};
   return {E, false};
 }
+std::pair<CVC4::Expr, bool> SubsumeBinaryAnd(CVC4::SmtEngine &SMT, CVC4::Expr E) {
+  if (E.getNumChildren() != 2)
+    return {E, false};
+  assert(E.getKind() == CVC4::Kind::AND);
 
+  auto L = E.getChildren()[0];
+  auto R = E.getChildren()[1];
+
+  auto Result = SMT.query(L.impExpr(R));
+  if (Result.isValid())
+    return {L, true};
+  Result = SMT.query(R.impExpr(L));
+  if (Result.isValid())
+    return {R, true};
+  return {E, false};
+}
+std::pair<CVC4::Expr, bool> SubsumeBinaryOr(CVC4::SmtEngine &SMT, CVC4::Expr E) {
+  if (E.getNumChildren() != 2)
+    return {E, false};
+  assert(E.getKind() == CVC4::Kind::OR);
+
+  auto L = E.getChildren()[0];
+  auto R = E.getChildren()[1];
+
+  auto Result = SMT.query(L.impExpr(R));
+  if (Result.isValid())
+    return {R, true};
+  Result = SMT.query(R.impExpr(L));
+  if (Result.isValid())
+    return {L, true};
+  return {E, false};
+}
+
+#define F(f) std::bind(f, std::ref(SMT), std::placeholders::_1)
 
 CVC4::Expr ApplyAllRecursively(CVC4::SmtEngine &SMT, CVC4::Expr E) {
   //Probably too slow to be practical beyond small programs
-  using namespace std::placeholders;
-  auto AndMutator = std::bind(ShortCircuitBinaryAnd, std::ref(SMT), _1);
-  auto OrMutator = std::bind(ShortCircuitBinaryOr, std::ref(SMT), _1);
-  
-  E = PostOrderKindMatch(E, CVC4::Kind::AND, AndMutator);
-  E = PostOrderKindMatch(E, CVC4::Kind::OR, OrMutator);
+  E = PostOrderKindMatch(E, CVC4::Kind::AND, F(ShortCircuitBinaryAnd));
+  E = PostOrderKindMatch(E, CVC4::Kind::OR, F(ShortCircuitBinaryOr));
+  E = PostOrderKindMatch(E, CVC4::Kind::AND, F(SubsumeBinaryAnd));
+  E = PostOrderKindMatch(E, CVC4::Kind::OR, F(SubsumeBinaryOr));
   
   return E;
 }
